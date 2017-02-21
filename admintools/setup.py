da4955b5d47291636ca52723073cec62bc0ab32f
docent2010 <docent2010@ukr.net>
2017-02-21 12:54:34 +0200

    Пароль + файл настроек

--- admintools/setup.py
+++ admintools/setup.py
@@ -11,24 +11,25 @@
             Параметр:
                 имя клиента
 """
-
+import getpass
+import json
 import sys
 import requests
+import hashlib
 
-
-url = "http://hive.product.in.ua:8885/api"
 headers = {"content-type": "application/json"}
-
+url = json.load(open("setup-config.json", "r"))["api"]["url"]
 command_list = ["register"]
 
+pw = getpass.getpass("Enter password: ")
+pw = hashlib.md5(str.encode(pw))
 
 if len(sys.argv) == 3 and sys.argv[1] in command_list:
-    data = {"action": sys.argv[1], "name": sys.argv[2]}
+    data = {"action": sys.argv[1], "name": sys.argv[2], "key":pw.hexdigest()}
     r = requests.post(url, headers=headers, json=data)
     print(r.json(),sep=",", end="\n")
 else:
     print(__doc__)
-input("Для завершения нажмите ENTER")
-sys.exit()
+
 
 
