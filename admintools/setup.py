1ff834cc7f44c23437f6f0029285e4554a91ff05
simersim <simer@f1.mk.ua>
2017-02-21 14:22:51 +0200

    Исправлен порядок запроса пароля

--- admintools/setup.py
+++ admintools/setup.py
@@ -1,16 +1,21 @@
-
-"""Первичная регистрация имени рабочего места
-******************************************
-  **Скрипт установки клиента setup.py**
-
-    setup.py [команда [параметр]]
-
-            Команда регистрации клиента:
-                register имя_клиента
-
-            Параметр:
-                имя клиента
 """
+ _____               _     _       _____     ___ _
+|     |___ _____ ___| |___| |_ ___|   __|___|  _| |_
+|   --| . |     | . | | -_|  _| -_|__   | . |  _|  _|
+|_____|___|_|_|_|  _|_|___|_| |___|_____|___|_| |_|
+                |_|
+
+          ***Скрипт установки клиента***
+
+setup.py [команда [параметр]]
+
+    Команда регистрации клиента:
+        register имя_клиента
+
+    Параметр:
+        имя клиента
+"""
+
 import getpass
 import json
 import sys
@@ -21,10 +26,9 @@ headers = {"content-type": "application/json"}
 url = json.load(open("setup-config.json", "r"))["api"]["url"]
 command_list = ["register"]
 
-pw = getpass.getpass("Enter password: ")
-pw = hashlib.md5(str.encode(pw))
-
 if len(sys.argv) == 3 and sys.argv[1] in command_list:
+    pw = getpass.getpass("Enter password: ")
+    pw = hashlib.md5(str.encode(pw))
     data = {"action": sys.argv[1], "name": sys.argv[2], "key":pw.hexdigest()}
     r = requests.post(url, headers=headers, json=data)
     print(r.json(),sep=",", end="\n")
