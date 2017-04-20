11a54ea5371b09aa0dd962011ff2480399766344
docent2010 <docent2010@ukr.net>
2017-04-20 20:21:40 +0300

    2017042001
    Рабочая версия клиента

--- client/lib.py
+++ client/lib.py
@@ -6,9 +6,15 @@ import hashlib
 import psutil
 import copy
 import ctypes
-import logging, logging.handlers
+import time
+import logging
 
 log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+filehandler = logging.handlers.RotatingFileHandler('client_log.log', encoding='utf8', maxBytes=100000, backupCount=1)
+formatter = logging.Formatter(fmt='[%(asctime)-19s] # %(levelname)-8s # %(message)-40s  #%(filename)s[LINE:%(lineno)d]#', datefmt='%Y-%m-%d %H:%M:%S')
+filehandler.setFormatter(formatter)
+log.addHandler(filehandler)
 
 CONFIG_FILE = json.load(open("config.json", "r"))
 
@@ -25,35 +31,37 @@ def request_comm(url=CONFIG_FILE["url"], data=None, file_path=None):
         r = requests.post(url)
         with open(local_archive_name, "wb") as code:
             code.write(r.content)
+            log.info("download - pass")
         return local_archive_name
 
 
-def search_update(ext_app_dict):
-    local_app_dict = [app for app in CONFIG_FILE["software"] if app["name"] == ext_app_dict["name"]][0]
-    zip_file = os.path.join(CONFIG_FILE["tmp_dir"], os.path.split(ext_app_dict["url"])[1])
-
-    # Taskkill all processes from local_path dir
-    task_killer(local_app_dict)
-
-
+def backup_update(app_dict):
+    # path 1-loc app_conf, 2-path for zipper
+    local_app_dict = [app for app in CONFIG_FILE["software"] if app["name"] == app_dict["name"]][0]
+    zip_file = os.path.join(CONFIG_FILE["tmp_dir"], os.path.split(app_dict["url"])[1])
 
     # Create backup.zip
     arch_path = os.path.join(CONFIG_FILE["backup_dir"], "{0}_{1}".format(local_app_dict["name"], local_app_dict["version"]))
-    shutil.make_archive(arch_path, format='zip', root_dir=local_app_dict["local_path"])
+    file_name = shutil.make_archive(arch_path, format='zip', root_dir=local_app_dict["local_path"])
 
-    # Unzip and update file(s)
-    shutil.unpack_archive(zip_file, extract_dir=local_app_dict["local_path"], format="zip")
-    return local_app_dict["local_path"]
+    if file_name:
+        # Unzip and update file(s)
+        shutil.unpack_archive(zip_file, extract_dir=local_app_dict["local_path"], format="zip")
+        log.info("files update - pass")
+        return local_app_dict["local_path"]
+    else:
+        raise Exception("backup - FAIL")
 
 
 def config_update(ext_app_dict):
-    config = json.load(open("config.json", "r"))
-    for app in config["software"]:
+    # config = json.load(open("config.json", "r"))
+    for app in CONFIG_FILE["software"]:
         if app["name"] == ext_app_dict["name"]:
             app["version"] = ext_app_dict["version"]
             break
-        with open("config.json", "w") as file:
-            json.dump(config, file, indent=4, sort_keys=True)
+    with open("config.json", "w") as file:
+        json.dump(CONFIG_FILE, file, indent=4, sort_keys=True)
+    log.info("app \"{}\" config - pass".format(app["name"]))
     return 0
 
 
@@ -70,7 +78,7 @@ def hash_control(file_path, hash_income):
 
 
 def task_killer(app_dict):
-    local_app_dict = copy.deepcopy(app_dict)
+    local_app_dict = copy.deepcopy([app for app in CONFIG_FILE["software"] if app["name"] == app_dict["name"]][0])
     process_id = [proc for proc in psutil.process_iter()]
     for pid in process_id:
         try:
@@ -78,6 +86,49 @@ def task_killer(app_dict):
         except Exception:
             path = ""
         if path and ((local_app_dict["local_path"] + "\\") in (os.path.split(path)[0] + "\\")):
-            ctypes.windll.user32.MessageBoxW(0, "Закройте приложение \"{}\" и нажмите ОК".format(local_app_dict["name"]), "ОБНОВЛЕНИЕ ПРИЛОЖЕНИЯ", 4112)
-            pid.kill()
+            if local_app_dict['dialog']==1:
+                while psutil.pid_exists(pid.pid):
+                    answ = ctypes.windll.user32.MessageBoxW(0, "Закройте приложение \"{}\" и нажмите ОК".format(pid.name()), "ОБНОВЛЕНИЕ ПРИЛОЖЕНИЯ", 4112)
+            else:
+                pid.kill()
     return 0
+
+
+def check_pid():
+    if not os.path.isfile(os.path.join(os.getcwd(), 'rub.pid')):
+        return (0,0)
+    with open('run.pid', 'r') as f_pid:
+        pid = f_pid.read()
+    if pid:
+        try:
+            f = psutil.Process(pid).exe()
+        except Exception:
+            #tuple describe [0]-pid in file, [1]-pid in process
+            return(pid, 0)
+        else:
+            if f in os.getcwd():
+                log.info("Updater on the work - second Updater stopped")
+                sys.exit(1)
+    else:
+        #tuple describe: [0]-pid in file, [1]-pid in process
+        return (pid, 0)
+
+
+def set_pid(reset=False):
+    if reset:
+        pid=""
+    else:
+        pid = os.getpid()
+    with open('run.pid', 'w') as f:
+        f.write(str(pid))
+    return pid
+
+
+
+if __name__=='__main__':
+    print(check_pid())
+
+
+
+
+
--- client/updater.py
+++ client/updater.py
@@ -1,7 +1,7 @@
 import json
 import os, sys
 import shutil
-import logging
+import logging.handlers
 import ctypes
 import time
 from client.lib import *
@@ -10,13 +10,14 @@ from client.lib import *
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 filehandler = logging.handlers.RotatingFileHandler('client_log.log', encoding='utf8', maxBytes=100000, backupCount=1)
-formatter = logging.Formatter(fmt='[%(asctime)-19s] # %(levelname)-8s #'
-            '  %(message)s  #%(filename)s[LINE:%(lineno)d]#', datefmt='%Y-%m-%d %H:%M:%S')
+formatter = logging.Formatter(fmt='[%(asctime)-19s] # %(levelname)-8s # %(message)-40s  #%(filename)s[LINE:%(lineno)d]#', datefmt='%Y-%m-%d %H:%M:%S')
 filehandler.setFormatter(formatter)
 log.addHandler(filehandler)
 
-resp = request_comm(data={"action":"alive", "id": CONFIG_FILE["id"], "timestamp": int(time.time())})
+check_pid()
+set_pid(reset=False)
 
+resp = request_comm(data={"action":"alive", "id": CONFIG_FILE["id"], "timestamp": int(time.time())})
 
 while resp["status"] != "None":
 
@@ -25,12 +26,15 @@ while resp["status"] != "None":
         resp = request_comm(data=data)
 
     if resp["status"] == "update":
+        # dict for recording update process
         summary = dict.fromkeys([k["name"] for k in resp["software"]])
+
         for app in resp["software"]:
             try:
                 loc_archive = request_comm(url=app["url"], file_path=CONFIG_FILE["tmp_dir"])
                 hash_control(loc_archive, app["hash"])
-                search_update(app)
+                task_killer(app)
+                backup_update(app)
                 config_update(app)
             except Exception :
                 log.critical(sys.exc_info()[0:2])
@@ -38,11 +42,14 @@ while resp["status"] != "None":
                 summary[app['name']]="Crash update"
                 continue
             else:
-                log.debug(" \"{}\" => Successful update".format(app['name']))
+                ctypes.windll.user32.MessageBoxW(0, "Приложение \"{}\" обновлено.\r\nНажмите ОК".format(app["name"]), "ОБНОВЛЕНИЕ ПРИЛОЖЕНИЯ", 4160)
+                log.debug("\"{}\" => Successful update".format(app['name']))
                 summary[app['name']]="Successful update"
         log.info("*Summed updating:*")
         for k in summary:
-            log.info("**{} => {}".format(k, summary[k]))
+            log.info("***{} => {}".format(k, summary[k]))
+        set_pid(reset=True)
 
+    resp = request_comm(data={"action": "alive", "id": CONFIG_FILE["id"], "timestamp": int(time.time())})
 
 
